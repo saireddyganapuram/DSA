1efdd7af5a5f10d386e760be0cfc75499a7b4e7b
Sai Reddy Ganapuram <gsaireddy2005@gmail.com>
2025-07-16 23:22:09 +0530

    16-07-2025

--- minimizeTheHeights.py
+++ minimizeTheHeights.py
@@ -1,1 +1,16 @@
-arr = [3,9,12,]
+def minimizeHeights(arr , k):
+    arr.sort()
+    n = len(arr)
+    res = arr[n-1] - arr[0]
+    for i in range(1,n):
+        if(arr[i] - k < 0):
+            continue
+        minH = min(arr[0] + k,arr[i] - k)
+        maxH = max(arr[i-1] + k,arr[n-1] - k)
+        res = min(res,maxH - minH)
+    return res 
+
+arr = [3,9,12,16,20]
+k = 3
+res = minimizeHeights(arr,k)
+print(res)
